08e62a9ed056c27f33c009f063917e75ce494c36
Juni Brosas <juni@plxrmedia.com>
2024-03-09 15:42:24 +0800

    dynamic metadata

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -8,6 +8,25 @@ type Props = {
   }
 }
 
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+
+export async function generateMetadata({
+  params
+}: Props) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
+  const data = (await res.json() as Post);
+
+  return {
+    title: data.title
+  }
+}
+
 export default function page({ params, searchParams }: Props) {
   return (
     <div>
